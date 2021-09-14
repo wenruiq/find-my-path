f740997597e1a18e08222ba87fc1c3f38cf27b5b
wenruiq <58852708+wenruiq@users.noreply.github.com>
2021-09-14 13:22:33 +0800

    connected firestore to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import "package:flutter/material.dart";
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+import "./screens/chat_screen.dart";
+import "./screens/splash_screen.dart";
 
-class MyApp extends StatelessWidget {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Find My Path", theme: ThemeData());
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            //TODO: handle error
+            print("Firebase init error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+                title: "Find My Path",
+                theme: ThemeData(primarySwatch: Colors.blue),
+                home: const ChatScreen());
+          }
+          return const SplashScreen();
+        });
   }
 }
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (ctx, index) => Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text("This works!"))),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection("chats/4Dg0tTMeuDdYcEAIXNwy/messages")
+                .snapshots()
+                .listen((data) {
+              print(data['text']);
+            });
+          }),
+    );
+  }
+}
