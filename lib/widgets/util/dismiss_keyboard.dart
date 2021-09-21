2ccb74d30574cf7f9dfeb5944769280e0458a9d3
lifuhuang97 <54570187+lifuhuang97@users.noreply.github.com>
2021-09-21 11:04:30 +0800

    added widget to help dismiss keyboard

---
+++ lib/widgets/util/dismiss_keyboard.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+//* This widget helps to dismiss keyboard when user clicks on anywhere outside of input field
+class DismissKeyboard extends StatelessWidget {
+  final Widget child;
+
+  const DismissKeyboard({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          FocusManager.instance.primaryFocus!.unfocus();
+        }
+      },
+      child: child,
+    );
+  }
+}
