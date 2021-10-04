db229d8d754066e9603a7de6f3937801562f3c52
wenruiq <58852708+wenruiq@users.noreply.github.com>
2021-10-04 08:17:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:find_my_path/screens/query_loading_screen.dart';
 import "package:flutter/material.dart";
 import 'package:firebase_core/firebase_core.dart';
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import "./theme/custom_theme.dart";
 import "./screens/chat_screen.dart";
@@ -14,9 +15,10 @@ import './screens/vo_home_screen.dart';
 import './screens/assignments_screen.dart';
 import './screens/ratings_screen.dart';
 
-void main() {
+void main() async {
   //* Ensures Firebase initialized bef. running app
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
@@ -32,37 +34,36 @@ class _MyAppState extends State<MyApp> {
   //* Initialize Firebase
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
+  void initState() {
+    super.initState();
+    // FirebaseMessaging messaging = FirebaseMessaging.instance;
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   print('Got a message whilst in the foreground!');
+    //   print('Message data: ${message.data}');
+
+    //   if (message.notification != null) {
+    //     print('Message also contained a notification: ${message.notification}');
+    //   }
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "Find My Path",
         theme: customTheme,
         //* FutureBuilder for Firebase init
-        home: FutureBuilder(
-          future: _initialization,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              //TODO: Error handling
-              print("Firebase init error");
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              //* StreamBuilder listens to authStateChanges
-              return StreamBuilder(
-                  stream: FirebaseAuth.instance.authStateChanges(),
-                  builder: (ctx, userSnapshot) {
-                    if (userSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return const SplashScreen();
-                    }
-                    if (userSnapshot.hasData) {
-                      return const HomeScreenVI();
-                    }
-                    return const AuthScreen();
-                  });
-            }
-            return const SplashScreen();
-          },
-        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+              if (userSnapshot.hasData) {
+                return const HomeScreenVO();
+              }
+              return const AuthScreen();
+            }),
         routes: {
           HomeScreenVI.routeName: (context) => const HomeScreenVI(),
           HomeScreenVO.routeName: (context) => const HomeScreenVO(),
