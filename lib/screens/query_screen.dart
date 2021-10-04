f8e590c89181e0c9443f5b3127f1ee949bb6e3ba
lifuhuang97 <lifuhuang97@gmail.com>
2021-10-04 09:10:32 +0800

    updated query screen to add assignment to firebase

--- lib/screens/query_screen.dart
+++ lib/screens/query_screen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
 
 import "../widgets/util/dismiss_keyboard.dart";
 import "../widgets/query/current_location.dart";
@@ -13,6 +15,90 @@ class QueryScreen extends StatefulWidget {
 }
 
 class _QueryScreenState extends State<QueryScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  CollectionReference assignments =
+      FirebaseFirestore.instance.collection('assignments');
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  late FocusNode myFocusNode;
+
+  String _uid = '123';
+  String _displayName = "abc";
+
+  var _isLoading = false;
+  var _endLocation = "";
+
+  final TextEditingController _endLocationController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _uid = _auth.currentUser!.uid;
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _setLoading(bool isLoading) {
+    _isLoading = isLoading;
+  }
+
+  void _trySubmit() {
+    _formKey.currentState!.save();
+    _submitQueryForm(endLocation: _endLocation.trim());
+  }
+
+  void _submitQueryForm({required String endLocation}) async {
+    await users.doc(_uid).get().then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        print("user detail retrieved");
+        _displayName = documentSnapshot.get('displayName');
+      }
+    });
+
+    //* To be updated to be filled with all real info
+    var assignmentInfo = {
+      "VO_ID": "123",
+      "VI_ID": _uid,
+      "date": DateTime.now(),
+      "imageURL":
+          "https://images.unsplash.com/photo-1572281158640-30040dd70cbe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3024&q=80",
+      "currentLocation": {
+        "long": 103.85016229678607,
+        "lat": 1.2963557161277992,
+        "name": "Singapore Management University"
+      },
+      "endLocation": {
+        "long": 103.85113602626875,
+        "lat": 1.3006504987053975,
+        "name": endLocation
+      },
+      "status": "In Progress",
+      "VO_displayName": "Harvey",
+      "VI_displayName": _displayName,
+      "timeTaken": null,
+    };
+
+    setState(() {
+      _setLoading(true);
+    });
+
+    await assignments
+        .add(assignmentInfo)
+        .then((value) => print("Assignment added to Firestore"))
+        .catchError((error) => print("Failed to add assignment: $error"));
+
+    setState(() {
+      _setLoading(false);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DismissKeyboard(
@@ -30,6 +116,7 @@ class _QueryScreenState extends State<QueryScreen> {
               const CurrentLocation(),
               const SizedBox(height: 25),
               Form(
+                key: _formKey,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Column(
@@ -43,9 +130,14 @@ class _QueryScreenState extends State<QueryScreen> {
                             color: Theme.of(context).primaryColor),
                       ),
                       TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: "Enter your destination"),
-                      ),
+                          key: const ValueKey('endLocation'),
+                          controller: _endLocationController,
+                          decoration: const InputDecoration(
+                              hintText: "Enter your destination"),
+                          onSaved: (value) {
+                            _endLocation = value.toString();
+                            print(_endLocation);
+                          }),
                       const SizedBox(
                         height: 20,
                       ),
@@ -89,8 +181,10 @@ class _QueryScreenState extends State<QueryScreen> {
                   width: MediaQuery.of(context).size.width * 0.8,
                   height: 50,
                   child: ElevatedButton(
-                    onPressed: () =>
-                        Navigator.pushNamed(context, "/queryloading"),
+                    onPressed: () {
+                      _trySubmit();
+                      Navigator.pushNamed(context, "/queryloading");
+                    },
                     child: const Text(
                       "Send Request",
                       style: TextStyle(fontSize: 24),
